6bfb0461fe72fd9c4fa3e163717f9274b4c786c4
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-18 23:38:30 -0300

    Created Game project

---
+++ src/Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using Raylib_cs;
+
+public class Game {
+    private static Game _instance;
+    
+    private const string _windowTitle = "Snake for Two";   
+    public static int WindowWidth => 600;
+    public static int WindowHeight => 600;
+    public static int TargetFPS => 30;
+
+
+    private Game() {
+        _instance = this;
+        Raylib.InitWindow(WindowWidth, WindowHeight, _windowTitle);
+        Raylib.SetTargetFPS(TargetFPS);
+    }
+
+    static void Main() {
+        new Game();
+        _instance.GameLoop();
+        _instance.End();
+
+    }
+
+    private void GameLoop() {
+        while(!Raylib.WindowShouldClose()) {
+            Update();
+            Render();
+        }
+    }
+
+    private void Update() {
+
+    }
+
+    private void Render() {
+        Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Beige);
+        Raylib.EndDrawing();
+    }
+
+    private void End() {
+        Raylib.CloseWindow();
+    }
+
+}
