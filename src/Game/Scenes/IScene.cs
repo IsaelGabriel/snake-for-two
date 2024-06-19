327b28776cd9eca11ca00c06f31e2336e74f187a
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-18 23:44:07 -0300

    Add IScene interface

---
+++ src/Game/Scenes/IScene.cs
@@ -0,0 +1,12 @@
+using Raylib_cs;
+
+public interface IScene
+{
+    public Color ClearColor { get; }
+
+    public void Start();
+    public void Update();
+    public void End();
+    public void Render();
+
+}
