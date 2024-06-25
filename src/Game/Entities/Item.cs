55e37279419c43c7832a44d1ac6e172cf18f3263
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-25 19:38:54 -0300

    Add animation to items

--- src/Game/Entities/Item.cs
+++ src/Game/Entities/Item.cs
@@ -8,7 +8,10 @@ public enum ItemType {
 
 public class Item(ItemType itemType, int x, int y, MainScene parentScene) : CellEntity(x, y, parentScene)
 {
+    private const float _maxOffsetY = (float) 0.75f;
+    private const float _animationDuration = 1f;
     public readonly ItemType type = itemType;
+    private float _animationCount = Raylib.GetFrameTime();
 
     public override void Start()
     {
@@ -17,7 +20,6 @@ public class Item(ItemType itemType, int x, int y, MainScene parentScene) : Cell
 
     public override void Render()
     {
-        Vector2 position = new Vector2(x, y) * MainScene.TileSize + Vector2.One * 4;
         Vector2 size = Vector2.One * (MainScene.TileSize - 8);
         Color color = Color.Gray;
         switch(type) {
@@ -29,6 +31,19 @@ public class Item(ItemType itemType, int x, int y, MainScene parentScene) : Cell
                 break;
             default: break;
         }
-        Raylib.DrawRectangleV(position, size, color);
+        Raylib.DrawRectangleV(GetRenderPosition(), size, color);
+    }
+
+    private Vector2 GetRenderPosition() {
+        _animationCount += Raylib.GetFrameTime();
+        if(_animationCount > _animationDuration) _animationCount = 0f;
+
+        float t = _animationCount / _animationDuration;
+
+        Vector2 position = new Vector2(x, y) * MainScene.TileSize + Vector2.One * 4;
+        
+        position.Y -= _maxOffsetY * (float) Math.Sin(t  * (float) Math.PI * 2f) * (1 - t / 2);
+
+        return position;
     }
 }
