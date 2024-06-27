03d445b36b2aacbbefde15018e6c64c95b1614d8
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-27 01:12:33 -0300

    Add colors to portals

--- src/Game/Entities/Portal.cs
+++ src/Game/Entities/Portal.cs
@@ -3,14 +3,21 @@ using Raylib_cs;
 
 public class Portal(Vector2[] points, MainScene parentScene) : CellEntity(0, 0, parentScene)
 {
+    private readonly Color[] _colors = [
+        Color.Blue,
+        Color.Orange,
+        Color.Green,
+        Color.Purple
+    ];
+
     public Vector2[] points = points;
 
     public override void Render()
     {
         Rectangle rect = new(new(0, 0), Vector2.One * MainScene.TileSize);
-        foreach(Vector2 point in points) {
-            rect.Position = point * MainScene.TileSize;
-            Raylib.DrawRectangleLinesEx(rect, 3, Color.RayWhite);
+        for(int i = 0; i < points.Length; i++) {
+            rect.Position = points[i] * MainScene.TileSize;
+            Raylib.DrawRectangleLinesEx(rect, 3, _colors[i % _colors.Length]);
         }
     }
 
